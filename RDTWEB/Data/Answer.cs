2144030aeb91bd7097f9007003a937f5fdf04e4c
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2021-06-08 00:03:10 +0700

    validate null browser file

--- RDTWEB/Data/Answer.cs
+++ RDTWEB/Data/Answer.cs
@@ -18,6 +18,6 @@ namespace RDTWEB.Data
         public int QuestionId { get; set; }
         public virtual Question Question { get; set; }
         [NotMapped]
-        public IBrowserFile BrowserFile { get; set; }
+        public IBrowserFile? BrowserFile { get; set; } = null;
     }
 }
