85cf9a6b67944559b82a9327187cd4fd0782356b
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2021-06-08 13:08:00 +0700

    can download

---
+++ RDTWEB/Controllers/StorageController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace RDTWEB.Controllers
+{
+    public class StorageController : Controller
+    {
+        public IWebHostEnvironment Env { get; }
+        
+        public StorageController(IWebHostEnvironment env)
+        {
+            Env = env;
+        }
+
+        [Route("Storage/{**path}")]
+        public ActionResult Index(string path)
+        {
+            var absolutePath = Path.Combine(Env.ContentRootPath, "Storage", path);
+
+            var split = path.Split("/");
+            var filename = split.Last();
+
+            byte[] bytes = System.IO.File.ReadAllBytes(absolutePath);
+
+            return File(bytes, "application/force-download", filename);
+        }
+    }
+}
