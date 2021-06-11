fe6e2b68679fc44f3de095bc466852be03c23256
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2021-06-11 13:18:46 +0700

    install mudblazor

--- RDTWEB/Startup.cs
+++ RDTWEB/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using MudBlazor.Services;
 using RDTWEB.Areas.Identity;
 using RDTWEB.Models;
 
@@ -43,6 +44,7 @@ namespace RDTWEB
                 .AddScoped<AuthenticationStateProvider,
                     RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
             services.AddDatabaseDeveloperPageExceptionFilter();
+            services.AddMudServices();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
